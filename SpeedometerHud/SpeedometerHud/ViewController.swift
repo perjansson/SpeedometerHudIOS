b54ec130c9142f3746e6d8c907b77ce3acb701ae
Per Jansson <per.r.jansson@gmail.com>
2014-12-07 21:42:32 +0100

    Swipe gesture

--- SpeedometerHud/SpeedometerHud/ViewController.swift
+++ SpeedometerHud/SpeedometerHud/ViewController.swift
@@ -7,22 +7,86 @@
 //
 
 import UIKit
+import QuartzCore
+import Darwin
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var speed: UILabel!
     @IBOutlet weak var unit: UILabel!
     
+    var hasReceivedSpeed : Bool!
+    var isMirrored : Bool!
+    var isMph : Bool!
+    
+    let π = M_PI
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        hasReceivedSpeed = false
+        isMirrored = false
+        isMph = true
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: "swipeUpOrDown")
+        swipeUp.numberOfTouchesRequired = 1
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: "swipeUpOrDown")
+        swipeDown.numberOfTouchesRequired = 1
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        view.addGestureRecognizer(swipeDown)
+        
+        UIApplication.sharedApplication().idleTimerDisabled = true
+        UIApplication.sharedApplication().setStatusBarHidden(true, withAnimation: UIStatusBarAnimation.Fade)
+        
+        /*var alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
+            switch action.style {
+            case .Default:
+                println("default")
+                
+            case .Cancel:
+                println("cancel")
+                
+            case .Destructive:
+                println("destructive")
+            }
+        }))
+        self.presentViewController(alert, animated: true, completion: nil)*/
+    }
+    
+    func swipeUpOrDown() {
+        if (isMirrored!) {
+            unmirrorScreen()
+        } else {
+            mirrorScreen()
+        }
+    }
+    
+    func mirrorScreen() {
+        isMirrored = true
+        UIView.beginAnimations(nil, context: nil)
+        speed.layer.transform = CATransform3DMakeRotation(CGFloat(π), -360.0, 1.0, 0.0)
+        unit.layer.transform = CATransform3DMakeRotation(CGFloat(π), -360.0, 1.0, 0.0)
+        UIView.commitAnimations()
+    }
+    
+    func unmirrorScreen() {
+        isMirrored = false
+        UIView.beginAnimations(nil, context: nil)
+        speed.layer.transform = CATransform3DMakeRotation(CGFloat(π), 0.0, 0.0, 0.0)
+        unit.layer.transform = CATransform3DMakeRotation(CGFloat(π), 0.0, 0.0, 0.0)
+        UIView.commitAnimations()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        UIApplication.sharedApplication().idleTimerDisabled = false
     }
-
 
 }
 
