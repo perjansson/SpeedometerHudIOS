a3465cfc59bcc773bdefa76c953a94b0dec42f3b
Per Jansson <per.r.jansson@gmail.com>
2014-12-07 19:19:22 +0100

    Initial commit

---
+++ SpeedometerHud/SpeedometerHud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeedometerHud
+//
+//  Created by Per Jansson on 2014-12-07.
+//  Copyright (c) 2014 Per Jansson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
