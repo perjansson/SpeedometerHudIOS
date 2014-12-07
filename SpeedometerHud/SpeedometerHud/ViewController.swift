2b4fcc42f88000a29d601d819fa027d12676252e
Per Jansson <per.r.jansson@gmail.com>
2014-12-07 21:00:36 +0100

    showing waiting for gps...

--- SpeedometerHud/SpeedometerHud/ViewController.swift
+++ SpeedometerHud/SpeedometerHud/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var speed: UILabel!
+    @IBOutlet weak var unit: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
